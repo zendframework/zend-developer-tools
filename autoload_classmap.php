58d2f5078cb76291304a012f481d11ecc14b2e46
Evan Coury <me@evancoury.com>
2011-12-30 15:27:49 -0700

    Update classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,2 +1,7 @@
 <?php
-return array();
+return array(
+    'ZendDeveloperTools\Module'                              => __DIR__ . '/Module.php',
+    'ZendDeveloperTools\Service\DeveloperTools'              => __DIR__ . '/src/ZendDeveloperTools/Service/DeveloperTools.php',
+    'ZendDeveloperTools\Controller\DeveloperToolsController' => __DIR__ . '/src/ZendDeveloperTools/Controller/DeveloperToolsController.php',
+    'ZendDeveloperTools\View\Listener'                       => __DIR__ . '/src/ZendDeveloperTools/View/Listener.php',
+);
