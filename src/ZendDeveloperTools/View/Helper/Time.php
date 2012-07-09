9c6d5891ba844976a06afffc863ee5b772b13841
Eric <cossish@gmail.com>
2012-07-09 17:09:12 +0200

    Updated toolbar, added new entries and cleaned up

---
+++ src/ZendDeveloperTools/View/Helper/Time.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * ZendDeveloperTools
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    ZendDeveloperTools_View
+ * @subpackage Helper
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace ZendDeveloperTools\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * @category   Zend
+ * @package    ZendDeveloperTools_View
+ * @subpackage Helper
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Time extends AbstractHelper
+{
+    /**
+     * Returns the formated time.
+     *
+     * @param  integer|float $time
+     * @param  integer       $precision Will only be used for seconds.
+     * @return string
+     */
+    public function __invoke($time, $precision = 2)
+    {
+        if ($time >= 1) {
+            return sprintf('%.' . $precision . 'f s', $time);
+        } else {
+            return sprintf('%.0f ms', $time * 1000);
+        }
+    }
+}
