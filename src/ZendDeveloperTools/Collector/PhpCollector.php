b4ee735668e3f8a009e99193b7b48b29ee6e8585
Anthony Shrieman <ashireman@gmail.com>
2012-09-04 10:56:24 -0700

    Adding Collector and view

---
+++ src/ZendDeveloperTools/Collector/PhpCollector.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Zend Developer Tools for Zend Framework (http://framework.zend.com/)
+ *
+ * @link       http://github.com/zendframework/ZendDeveloperTools for the canonical source repository
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd New BSD License
+ * @package    ZendDeveloperTools
+ * @subpackage Collector
+ */
+
+namespace ZendDeveloperTools\Collector;
+
+use Zend\Mvc\MvcEvent;
+
+/**
+ * Database (Zend\Db) Data Collector.
+ *
+ * @category   Zend
+ * @package    ZendDeveloperTools
+ * @subpackage Collector
+ */
+class PhpCollector extends AbstractCollector implements CollectorInterface, AutoHideInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'php';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getPriority()
+    {
+        return 20;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function collect(MvcEvent $mvcEvent)
+    {
+        return;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function canHide()
+    {
+        if (!isset($this->profiler)) {
+            return false;
+        }
+
+        return true;
+    }
+}
