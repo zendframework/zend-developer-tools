cf81a6714eea75520392ed0c336d4fd7ce52529f
Sina miandashti <miandashti@gmail.com>
2013-03-26 03:26:02 -0700

    fix has zend\db instance bug

--- Module.php
+++ Module.php
@@ -204,7 +204,7 @@ class Module implements
                     $p  = false;
                     $db = new Collector\DbCollector();
 
-                    if ($sm->has('Zend\Db\Adapter\Adapter')) {
+                    if ($sm->has('Zend\Db\Adapter\Adapter',true)) {
                         $adapter = $sm->get('Zend\Db\Adapter\Adapter');
                         if ($adapter instanceof ProfilingAdapter) {
                             $p = true;
