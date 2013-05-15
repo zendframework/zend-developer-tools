6b05578418b586d0f8033470dc8a33ef10ce6f86
Marco Pivetta <ocramius@gmail.com>
2013-05-16 00:47:08 +0200

    Removing unused views/listeners/collectors/styles, moving styles to toolbar.css

--- config/module.config.php
+++ config/module.config.php
@@ -5,35 +5,4 @@ return array(
             'zenddevelopertools' => __DIR__ . '/../view',
         ),
     ),
-    'controllers' => array(
-        'factories' => array(
-            'ZendDeveloperTools\\Controller\\Datadump' => 'ZendDeveloperTools\\Controller\\Factory\\DatadumpControllerFactory',
-        ),
-    ),
-    'router' => array(
-        'routes' => array(
-            'zend_developer_tools_config' => array(
-                'type' => 'Zend\\Mvc\\Router\\Http\\Literal',
-                'options' => array(
-                    'route' => '/zdt/request/config',
-                    'defaults' => array(
-                        'controller' => 'ZendDeveloperTools\\Controller\\Datadump',
-                        'action'     => 'show',
-                        'title'      => 'Loaded Configuration'
-                    ),
-                ),
-            ),
-            'zend_developer_tools_services' => array(
-                'type' => 'Zend\\Mvc\\Router\\Http\\Literal',
-                'options' => array(
-                    'route' => '/zdt/request/services',
-                    'defaults' => array(
-                        'controller' => 'ZendDeveloperTools\\Controller\\Datadump',
-                        'action'     => 'show',
-                        'title'      => 'Loaded Services'
-                    ),
-                ),
-            ),
-        ),
-    ),
 );
