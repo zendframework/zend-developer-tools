8b1ec7499f6ac70a4283384f3fccba2272e51f53
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-04-23 21:38:35 +0700

    remove unused AutoloaderProviderInterface

--- src/ZendDeveloperTools/Module.php
+++ src/ZendDeveloperTools/Module.php
@@ -16,7 +16,6 @@ use Zend\ModuleManager\Feature\InitProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
 use Zend\ModuleManager\Feature\BootstrapListenerInterface;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
 use BjyProfiler\Db\Adapter\ProfilingAdapter;
 
@@ -24,7 +23,6 @@ class Module implements
     InitProviderInterface,
     ConfigProviderInterface,
     ServiceProviderInterface,
-    AutoloaderProviderInterface,
     BootstrapListenerInterface,
     ViewHelperProviderInterface
 {
@@ -118,23 +116,9 @@ class Module implements
     /**
      * @inheritdoc
      */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     public function getViewHelperConfig()
