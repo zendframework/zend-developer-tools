a2554cfc888a5274b056c013b738461390527551
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-06-25 13:44:02 +0700

    update include module.config.php path

--- src/Module.php
+++ src/Module.php
@@ -122,7 +122,7 @@ class Module implements
      */
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getViewHelperConfig()
